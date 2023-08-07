91b97a8e6481a74a7e479cc0208a5edf142545ef
Harry <133057991+harharry@users.noreply.github.com>
2023-08-07 15:49:00 -0400

    Update Player.java

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Player.java
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Player.java
@@ -1,9 +1,13 @@
 /**
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+ * Name=Harry, Date=8/7/2023.
  */
 package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
@@ -11,18 +15,27 @@ package ca.sheridancollege.project;
  * @author dancye
  * @author Paul Bonenfant Jan 2020
  */
-public abstract class Player {
+public class Player {
 
     private String name; //the unique name for this player
+    
+    
+    List<Card> PlayerDeck=new ArrayList<Card>(52);
+    
 
     /**
      * A constructor that allows you to set the player's unique ID
      *
-     * @param name the unique ID to assign to this player.
+     * 
+     * @param Pd
      */
-    public Player(String name) {
-        this.name = name;
+    public Player(List<Card> Pd) {
+       for(int i=0;i<Pd.size();i++){
+           PlayerDeck.add(Pd.get(i));
+       }
     }
+    
+ 
 
     /**
      * @return the player name
@@ -44,6 +57,6 @@ public abstract class Player {
      * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
      * with logic to play your game.
      */
-    public abstract void play();
+    //public abstract void play();
 
 }
