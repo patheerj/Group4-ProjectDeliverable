43d6d16a13b3842d3639adfb13d8e76dd8da62d2
Harry <133057991+harharry@users.noreply.github.com>
2023-08-07 15:47:45 -0400

    Update GroupOfCards.java

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/GroupOfCards.java
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/GroupOfCards.java
@@ -1,12 +1,14 @@
 /**
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+ * Name=Harry, Date = 8/7/2023.
  */
 package ca.sheridancollege.project;
 
-import java.util.ArrayList;
+
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
@@ -16,13 +18,22 @@ import java.util.Collections;
  * @author Paul Bonenfant Jan 2020
  */
 public class GroupOfCards {
-
     //The group of cards, stored in an ArrayList
-    private ArrayList<Card> cards;
-    private int size;//the size of the grouping
+    private LinkedList<Card> card=generateCards();
+    
+    //the size of the grouping
+    private int size =26;
+    
+    List<Card> onePlayer=card.subList(0, size);
+    List<Card> twoPlayer=card.subList(size, (size*2));
+    
 
-    public GroupOfCards(int size) {
-        this.size = size;
+    public LinkedList<Card> getCard() {
+        return card;
+    }
+
+    public GroupOfCards() {
+        size=26;   
     }
 
     /**
@@ -30,26 +41,38 @@ public class GroupOfCards {
      *
      * @return the group of cards.
      */
-    public ArrayList<Card> getCards() {
-        return cards;
+    public LinkedList<Card> generateCards() {
+        LinkedList<Card> mydeck;
+        mydeck = new LinkedList<Card>();
+ 
+        for(Card.Suit s: Card.Suit.values())
+                {
+        for(Card.Value v: Card.Value.values())
+                    {
+                        Card c=new Card(s,v);
+                        mydeck.add(c);
+                        }
+                        
+                    }
+                
+        GroupOfCards.shuffle(mydeck);
+        
+        return mydeck;
+
     }
 
-    public void shuffle() {
-        Collections.shuffle(cards);
+    
+    public static void shuffle(LinkedList<Card> heer) {
+        Collections.shuffle(heer);
+    }
+    
+    public static LinkedList<Card> playing(LinkedList<Card> list){
+        int index=list.size()-1;
+        System.out.println(list.get(index));
+        list.remove(index); 
+        return list;
     }
 
-    /**
-     * @return the size of the group of cards
-     */
-    public int getSize() {
-        return size;
-    }
-
-    /**
-     * @param size the max size for the group of cards
-     */
-    public void setSize(int size) {
-        this.size = size;
-    }
-
+    
+     
 }//end class
